590a34094f405276c1f66d286bc4963c583f8c19
Valeria <valeriakirkbride@gmail.com>
2023-08-29 07:21:41 -0600

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity1, activity2 = 'roller-skate') {
+    return activity1 ? `This Saturday, I want to ${activity1}!` : `This Saturday, I want to ${activity2}!`;
+}
+function mondayWork(decision1, decision2 = 'go to the office.') {
+    return decision1 ? `This Monday, I will ${decision1}.` : `This Monday, I will ${decision2}`;
+}
+function wrapAdjective(param2 = '*') {
+    return function(param1 = 'special') {
+        return `You are ${param2}${param1}${param2}!`;
+    }
+}
